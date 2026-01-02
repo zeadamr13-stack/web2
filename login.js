7cd90fa2215488cb70adb37f3bf68422228b7de4
zeadamr13-stack <zeadamr13@gmail.com>
2026-01-02 04:26:25 +0200

    Update login.js

--- login.js
+++ login.js
@@ -6,8 +6,9 @@ const pass = document.getElementById("password").value;
 
 if (user === "admin" && pass === "1234") {
     localStorage.setItem("isLoggedIn", "true");
-    window.location.href = "index.html";
+    window.location.href = "home.html";
 } else {
     alert("بيانات الدخول غير صحيحة");
 }
 });
+
